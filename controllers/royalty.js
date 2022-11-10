dd3a389047e6fee7fc0bfb48de8c18d697b24657
damipop7 <S536877@nwmissouri.edu>
2022-11-10 16:53:40 -0600

    completed the lab up till part 2

--- controllers/royalty.js
+++ controllers/royalty.js
@@ -12,14 +12,21 @@ exports.royalty_list = async function(req, res) {
     }   
 }; 
 // for a specific royalty. 
-exports.royalty_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: royalty detail: ' + req.params.id); 
+exports.royalty_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Royalty.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle royalty create on POST. 
-exports.royalty_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: royalty create POST'); 
-}; 
+//exports.royalty_create_post = function(req, res) { 
+//    res.send('NOT IMPLEMENTED: royalty create POST'); 
+//}; 
  
 // Handle royalty delete form on DELETE. 
 exports.royalty_delete = function(req, res) { 
@@ -37,6 +44,27 @@ exports.royalty_view_all_Page = async function(req, res) {
     try{ 
         theRoyaltys = await Royalty.find(); 
         res.render('Royalty', { title: 'Royalty Search Results', results: theRoyaltys }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
+// Handle royalty create on POST. 
+exports.royalty_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Royalty(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.name = req.body.name; 
+    document.country = req.body.country; 
+    document.wealth = req.body.wealth; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
     } 
     catch(err){ 
         res.status(500); 
